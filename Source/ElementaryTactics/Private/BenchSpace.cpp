f04b8de7e892ca55d21430f7b9847af405454574
Pipe123Azul <bustosbustospipe123@gmail.com>
2024-02-19 19:47:23 -0500

    [0.0.1.6]
    Funcion: BenchSpace
    
    Simulator.umap: Les agregamos el valor de los TriggerBox del campo de batalla
    Home.umap: Se cambio el nombre del nivel Menu a Home
    BenchSpace.cpp: Se añadio el codigo default de un actor
    BenchSpace.h: Se agregaron dos variables (float (Almacena las posiciones de los TriggerBox) Bool (Para saber si se encuentra un actor dentro de un TriggerBox))
    ClickErrorSound.wav: Sonido del Click error

---
+++ Source/ElementaryTactics/Private/BenchSpace.cpp
@@ -0,0 +1,20 @@
+#include "BenchSpace.h"
+
+ABenchSpace::ABenchSpace()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ABenchSpace::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABenchSpace::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
