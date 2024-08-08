1f0c6542e0ea440b87bf250df4bf80a6cb40dde7
Pipe123Azul <bustosbustospipe123@gmail.com>
2024-08-07 20:17:50 -0500

    [0.0.1] Home Presentation
    UX/UI
    
    - NEW: Home Level
    - NEW: Fade Widget
    - NEW: Play Widget
    - NEW: Tittle Widget
    - NEW: Settings Widget
    - NEW: Video Widget
    
    ENGINE
    
    - NEW: Instance
    - NEW: AudioEngine
    - NEW: CastEngine
    - NEW: DevelopmentEngine
    - NEW: InterfaceEngine
    - NEW: MediaEngine
    - NEW: WidgetEngine
    
    USECASE
    
    - NEW: AudioTools
    - NEW: CastTools
    - NEW: InterfaceTools
    - NEW: MediaTools
    - NEW: WidgetTools

---
+++ Source/ElementaryTactics/ElementaryTactics.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ElementaryTactics.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ElementaryTactics, "ElementaryTactics" );
+
+DEFINE_LOG_CATEGORY(LogElementaryTactics)
+ 
