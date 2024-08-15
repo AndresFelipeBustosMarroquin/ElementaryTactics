89e40d02a7e375d5d3d5e8b201b74ed4b3cc6575
Pipe123Azul <bustosbustospipe123@gmail.com>
2024-08-14 19:12:18 -0500

    [0.0.3] Change Language
    UX/UI
    
    - UPD: SettingsWidget
    
    ENGINE
    
    - NEW: UtilitiesEngine
    
    USECASE
    
    - NEW: UtilitiesTools

--- Source/ElementaryTactics/ElementaryTactics.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ElementaryTactics.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ElementaryTactics, "ElementaryTactics" );
-
-DEFINE_LOG_CATEGORY(LogElementaryTactics)
- 
